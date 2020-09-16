7befa3efd52206e0cfe9c652bc90dbabe3fa8c43
226wyj <308033240@qq.com>
2020-09-16 18:52:01 +0800

    Finish SelectionSort.py

--- Python Version/Sort/SelectionSort.py
+++ Python Version/Sort/SelectionSort.py
@@ -1,14 +1,27 @@
-a = [1,2, 3, 5, 3, 2, 9, 28, 4, 6]
+class SelectionSort:
+    """选择排序，默认按升序排列"""
 
-def simple_selection_sort(a):
-    for i in range(len(a)): 
-        min = i                         # min记录的是最小值的下标
-        for j in range(i + 1, len(a)):
-            if a[min] >= a[j]:
-                min = j
-        temp = a[min]
-        a[min] = a[i]
-        a[i] = temp
-    return a   
+    def simpleSelectionSort(self, array):
+        for i in range(len(array)):
+            # min记录最小值的下标，每一次都选择待排序列中最小的一个元素，将其移动到序列最左边
+            min = i
+            for j in range(i + 1, len(array)):
+                if array[min] >= array[j]:
+                    min = j
+            temp = array[min]
+            array[min] = array[i]
+            array[i] = temp
+        return array
+    
+    def showResults(self, array):
+        print("----------------------------------------")
+        print("排序前: ")
+        print(array)
+        print("排序后: ")
+        print(self.simpleSelectionSort(array))
+        print("----------------------------------------")
 
-print(simple_selection_sort(a))   
+if __name__ == "__main__":
+    array = [1,2, 3, 5, 3, 2, 9, 28, 4, 6]
+    demo = SelectionSort()
+    demo.showResults(array)
