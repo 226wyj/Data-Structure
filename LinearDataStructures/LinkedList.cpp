f89f7a197a9cf5578ec878bbe23378f8bc745174
226wyj <308033240@qq.com>
2020-09-29 16:51:41 +0800

    2020/9/29-16:50 Rewrite the readme.md and finish deleteData and insertData methods of LinkedList.cpp

--- LinearDataStructures/LinkedList.cpp
+++ LinearDataStructures/LinkedList.cpp
@@ -7,7 +7,7 @@ struct Node {
     Node* next;
 };
 
-// 链表类
+// 链表定义
 class LinkedList {
 private:
     // 头节点
@@ -58,39 +58,55 @@ public:
         Node* prePointer = nullptr;
         int flag = 0;
         while (currentPointer != nullptr) {
+            // 找到data，将其删除
             if (currentPointer->data == data) {
                 if (currentPointer == this->head)
                     head = head->next;
-                else if (prePointer)
-                    prePointer->next = currentPointer->next;
+                else {
+                    if (prePointer == head)
+                        prePointer = currentPointer->next;
+                    else
+                        prePointer->next = currentPointer->next;
+                }
                 flag = 1;
                 break;
             }
-            prePointer = currentPointer;
-            currentPointer = currentPointer->next;
+            // 未找到data，则继续遍历
+            else {
+                prePointer = currentPointer;
+                currentPointer = currentPointer->next;
+            }
         }
         return flag;
     }
 
-    // 在链表特定位置中插入数据，index从0开始
+    // 在链表特定位置中插入数据，index从0开始，插入的数据作为链表中的第index个节点
     bool insertData(int index, int data) {
         if (index < 0 || index > this->getLength()) {
             cout << "Index Out of Range Error!" << endl;
             return false;
         }
         else {
+            // 生成新的数据节点
             Node* newNode = new(Node);
             newNode->data = data;
             newNode->next = nullptr;
-            Node* currentPointer = this->head;
-            for (int i = 0; i < index; i++) {
-                currentPointer = currentPointer->next;
+            // 找到待插入的位置
+            if (index == 0) {
+                Node* temp = head;
+                head = newNode;
+                newNode->next = temp;
             }
-            auto temp = currentPointer->next;
-            currentPointer->next = newNode;
-            newNode->next = temp;
+            else {
+                Node* currentPointer = this->head;
+                for (int i = 0; i < index - 1; i++) {
+                    currentPointer = currentPointer->next;
+                }
+                Node* temp = currentPointer->next;
+                currentPointer->next = newNode;
+                newNode->next = temp;
+            }
         }
-        
         return true;
     }
 
@@ -132,11 +148,15 @@ int main() {
     for (int i = 0; i < 5; i++) {
         myList.appendData(i);
     }
+    cout << "链表:" << endl;
     myList.showList();
-    cout << myList.getLength() << endl;
+    cout << "链表长度:  " << myList.getLength() << endl;
+    myList.deleteData(0);
+    cout << "删除元素0之后的链表:" << endl;
+    myList.showList();
+    cout << "链表长度:  " << myList.getLength() << endl;
 
-    cout << myList.deleteData(2) << endl;
+    myList.insertData(0, 100);
     myList.showList();
-    cout << myList.getLength() << endl;
     return 0;
 }
