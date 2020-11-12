7393946cae31ea577db8a3a6f6ddd8dc7691c265
226wyj <308033240@qq.com>
2020-11-12 19:20:53 +0800

    2020/11/12-19:20 finish AdjacencyMatrix.cpp

--- Graph/AdjacencyMatrix.cpp
+++ Graph/AdjacencyMatrix.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 // 邻接矩阵
@@ -8,38 +9,68 @@ private:
     vector<int> vertexes;
     vector<pair<int, int>> edges;
 public:
+    
+    int vertexNum;  // 顶点数量
 
     // 插入顶点
     void addVertex(int vertex) {
+        // 插入新的顶点之前首先要检查是否重复插入
+        for (auto v : vertexes) {
+            if (v == vertex)
+                throw "The vertex has already been inserted.";
+        }
         vertexes.push_back(vertex); 
+        // 插入顶点之后更新顶点数量
+        vertexNum = vertexes.size();
     }
 
     // 插入边
     void addArc(int source, int target) {
-        edges.push_back(make_pair(source, target));
+        // 插入新的边之前首先要检查是否重复插入
+        pair<int, int> newEdge = make_pair(source, target);
+        for (auto e : edges) {
+            if (e == newEdge)
+                throw "The vertex has already been inserted.";
+        }
+        edges.push_back(newEdge);
+    }
+
+    // 获取所有顶点
+    vector<int> getVertexes() {
+        return vertexes;
+    }
+
+    // 获取所有边
+    vector<pair<int, int>> getEdges() {
+        return edges;
     }
 
     // 构造邻接矩阵
-    pair<int**, int> createMatrix() {
-        int length = vertexes.size();
-        int matrix[length][length];
-        for (auto i : edges) {
-            int index1 = find(vertexes.begin(), vertexes.end(), i.first);
-            int index2 = find(vertexes.begin(), vertexes.end(), i.second);
+    int ** createMatrix() {
+        // 初始化矩阵
+        int **matrix = new int*[vertexNum]();
+        // 根据边的关系填充矩阵
+        for (int i = 0; i < vertexNum; i++) {
+            matrix[i] = new int[vertexNum]();
+        }
+        for (auto e : edges) {
+            vector<int>::iterator iter1 = find(vertexes.begin(), vertexes.end(), e.first);
+            vector<int>::iterator iter2 = find(vertexes.begin(), vertexes.end(), e.second);
+            int index1 = distance(vertexes.begin(), iter1);
+            int index2 = distance(vertexes.begin(), iter2);
             matrix[index1][index2] = 1;
         }
-        auto result = make_pair(matrix, length);
-        return result;      
+        return matrix;
     }
 
     //  展示结果
     void showGraph() {
-        int** matrix = createMatrix().first;
-        int length = createMatrix().second;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                cout << matrix[i][j];
+        auto matrix = createMatrix();
+        for (int i = 0; i < vertexNum; i++) {
+            for (int j = 0; j < vertexNum; j++) {
+                cout << matrix[i][j] << ' ';
             }
+            cout << endl;
         }
     }
 };
