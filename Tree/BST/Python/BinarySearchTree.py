41958cfa5cb6620d50a1b8fbf822dea35da2351b
226wyj <308033240@qq.com>
2022-03-29 00:14:44 -0400

    add delete-node method for BST.py

--- Tree/BST/Python/BinarySearchTree.py
+++ Tree/BST/Python/BinarySearchTree.py
@@ -12,7 +12,7 @@ class BinarySearchTree:
     def __init__(self) -> None:
         self.root = None
     
-    # 添加结点
+    # 添加节点
     def add_node(self, value):  
 
         def insert(root, value):
@@ -29,9 +29,38 @@ class BinarySearchTree:
         
         self.root = insert(self.root, value)
 
+    # 删除节点
+    def delete_node(self, key: int) -> str:
+        def find_min(root: Node) -> Node:
+            curr = root
+            while curr.left:
+                curr = curr.left
+            return curr
+        
+        def delete(root: Node, key: int) -> Node:
+            if not root:
+                return None
+            if root.value < key:
+                root.right = delete(root.right, key)
+            elif root.value > key:
+                root.left = delete(root.left, key)
+            else:
+                if not root.left:
+                    return root.right
+                if not root.right:
+                    return root.left
+                min_node = find_min(root.right)
+                root.value = min_node.value
+                root.right = delete(root.right, root.value)
+            return root
+        
+        if delete(self.root, key):
+            return "Successfully delete node {}.".format(key)
+        else:
+            return "Key {} does not exist.".format(key)
 
     # 层序遍历
-    def breadth_traverse(self) -> List[int]:
+    def level_order_traverse(self) -> List[int]:
         if not self.root:
             return
         queue = deque()
@@ -94,4 +123,6 @@ if __name__ == "__main__":
     nums = [50, 30, 10, 0, 20, 40, 70, 90, 100, 60, 80]
     for i in nums:
         binary_tree.add_node(i)
+    print(binary_tree.in_order_traverse())
+    print(binary_tree.delete_node(50))
     print(binary_tree.in_order_traverse())
