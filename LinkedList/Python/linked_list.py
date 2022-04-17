da2c5edcbfd1ae16131c59bd736bcd97325156c3
226wyj <308033240@qq.com>
2022-04-16 23:57:13 -0400

    finish python version linked-list

--- LinkedList/Python/linked_list.py
+++ LinkedList/Python/linked_list.py
@@ -1,15 +1,14 @@
-from multiprocessing import dummy
 from typing import List
 
 
-class Node:
+class Node(object):
     """ Definition of list node. """
     def __init__(self, val=0, next=None) -> None:
         self.val = val
         self.next = next
 
 
-class LinkedList:
+class LinkedList(object):
     """ Definition of linked list. """
     def __init__(self, head=None, tail=None, length=0) -> None:
         self.head = head
@@ -17,13 +16,21 @@ class LinkedList:
         self.length = length
     
     def __repr__(self) -> str:
-        return 'head={}, tail={}, length={}'.format(self.head.val, self.tail.val, self.length)
+        head_val = self.head.val if self.head else 'None'
+        tail_val = self.tail.val if self.tail else 'None'
+        return 'head={}, tail={}, length={}'.format(head_val, tail_val, self.length)
+
+    @staticmethod
+    def _del(node: Node) -> None:
+        """ Releases the memory allocated to the node object. """
+        node.next = None
+        del node
 
     def print_list(self) -> None:
         """ Prints all the node values of the linked list. """
         curr = self.head
         while curr:
-            end_symbol = ' -> ' if curr.next else ''
+            end_symbol = ' -> ' if curr.next else '\n'
             print(curr.val, end=end_symbol)
             curr = curr.next
     
@@ -58,16 +65,23 @@ class LinkedList:
 
     def remove(self, num: int) -> int:
         """ Removes a node based on the target value. """
-        dummy_head = Node(0, self.head)
+        if self.length == 0:
+            print("You can not remove anything from an empty linked list.")
+            return 0
         curr = self.head
-        pre = dummy_head
+        pre = None
         while curr:
             if curr.val == num:
+                # updates the head and tail pointers if necessary.
                 if curr == self.head:
                     self.head = curr.next
                 if curr == self.tail:
                     self.tail = pre
-                pre.next = curr.next
+                
+                # removes the target node.
+                if pre:
+                    pre.next = curr.next
+                    self._del(curr)
                 self.length -= 1
                 return 1
             else:
@@ -76,30 +90,40 @@ class LinkedList:
         return 0
 
     def insert(self, pos: int, num: int) -> None:
-        """ Inserts a node on the target position. """
-        if pos < 0 or pos > self.length + 1:
+        """ Inserts a node on the target position, pos should be within [0, length]. """
+        if pos < 0 or pos > self.length:
             print("The length of the list is {}, your pos is {}".format(self.length, pos))
-            print("The range of the pos parameter should be within [0, length + 1].")
+            print("The range of the pos parameter should be within [0, length].")
             return
+        # Uses a dummy_head node for assistance,
+        # so that we do not need to care about the corner case.
         dummy_head = Node(0, self.head)
+        
+        # Finds the target position.
         curr = dummy_head
-        while pos > 0:
+        for _ in range(pos, 0, -1):
             curr = curr.next
-            pos -= 1
+        
+        # Inserts the new node.
         new_node = Node(num, curr.next)
         curr.next = new_node
+
+        # Adjust the head and tail pointers if necessary.
         if pos == 0:
             self.head = new_node
-        if pos == self.length + 1:
+        if pos == self.length:
             self.tail = new_node
+        
+        # Updates length and releases the dummy_head node object.
         self.length += 1
-        dummy_head.next = None
-        del dummy_head
+        self._del(dummy_head)
 
     def reverse(self) -> None:
         """ Reverses the linked list. """
+        # Corner case: there is at most 1 node in the list.
         if not self.head or not self.head.next:
             return
+        # Uses two pointers to reverse the list in-place.
         pre, curr = None, self.head
         self.tail = self.head
         while curr:
--- LinkedList/Python/main.py
+++ LinkedList/Python/main.py
@@ -2,28 +2,52 @@ from linked_list import LinkedList
 
 
 if __name__ == "__main__":
+    # Creates a linked list object.
     linked_list = LinkedList()
 
-    print("Insert from the head: 0~4")
+    # Test 1. Appends 0-4 to the front.
+    print("\Append to the front: 0-4")
     for i in range(0, 5):
-        linked_list.add_node_at_head(i)
+        linked_list.append_front(i)
     linked_list.print_list()
+    print(linked_list)
 
-    print("Insert from the tail: 5~9")
+    # Test 2. Appends 5-9 to the back. 
+    print("\nAppend to the back: 5-9")
     for i in range(5, 10):
-        linked_list.add_node_at_tail(i)
+        linked_list.append_back(i)
     linked_list.print_list()
+    print(linked_list)
 
-    print("Length of the List: ", linked_list.length)
-
-    print("Remove the node whose value is 0:")
-    result = linked_list.remove_node(0)
+    # Test 3. Inserts a number at the first postion.
+    print("\nInsert number 10 at position 0:")
+    linked_list.insert(0, 10)
     linked_list.print_list()
+    print(linked_list)
 
-    print("Insert number 10 at position 0:")
-    linked_list.insert_node(0, 10)
+    # Test 4. Inserts a number at the last position.
+    print("\nInsert number 11 at position 11:")
+    linked_list.insert(11, 11)
     linked_list.print_list()
+    print(linked_list)
 
-    print("reverse the linked list:")
+    # Test 5. Inserts a number in the middle.
+    print("\nInsert number 12 at position 5:")
+    linked_list.insert(5, 12)
+    linked_list.print_list()
+    print(linked_list)
+
+    # Test 6. Reverse the linked list.
+    print("\nreverse the linked list:")
     linked_list.reverse()
     linked_list.print_list()
+    print(linked_list)
+
+    # Test 7. Removes all the nodes of the linked list.
+    print("\nRemove all the nodes whose values are from 0 to 12:")
+    for i in range(0, 13):
+        linked_list.remove(i)
+        linked_list.print_list()
+        print(linked_list)
+    
+    print()
